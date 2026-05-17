f2d81f35569a3a3912f13e745cd5b46773e51738
enzo405 <enzo.chaboisseau@gmail.com>
2026-05-17 16:32:13 +0200

    Add show_autoroles

--- bot/cogs/admin/autorole.py
+++ bot/cogs/admin/autorole.py
@@ -90,6 +90,52 @@ class Autorole(commands.Cog):
         except discord.errors.HTTPException as e:
             await interaction.response.send_message(e, ephemeral=True)
 
+    @app_commands.command(
+        name="show_autoroles",
+        description="Show all autoroles for the server",
+    )
+    @app_commands.checks.has_permissions(administrator=True)
+    async def show_autoroles(
+        self,
+        interaction: discord.Interaction,
+    ):
+        if not interaction.user.guild_permissions.administrator:
+            await interaction.response.send_message(
+                "Only server administrator can use this command", ephemeral=True
+            )
+            return
+
+        server_autoroles = self.db.select(
+            TableName.AUTOROLES.value,
+            where={"guild_id": interaction.guild.id},
+            order_by="threshold DESC",
+        )
+
+        # Build the response embed
+        embed = discord.Embed(
+            title="Autoroles",
+            description="Here are the autoroles for this server:",
+            color=discord.Color.blue(),
+        )
+        if not server_autoroles:
+            embed.add_field(
+                name="No autoroles",
+                value="There are no autoroles set up for this server.",
+                inline=False,
+            )
+        else:
+            embed.description = "Here are the autoroles for this server:\n\n"
+            for autorole in server_autoroles:
+                print(autorole)
+                role_id = autorole["role_id"]
+                threshold = autorole["threshold"]
+                embed.description += f"<@&{role_id}>: {threshold} points\n"
+
+        try:
+            await interaction.response.send_message(embed=embed)
+        except discord.errors.HTTPException as e:
+            await interaction.response.send_message(e, ephemeral=True)
+
 
 async def setup(bot):
     await bot.add_cog(Autorole(bot))
