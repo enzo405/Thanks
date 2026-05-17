ef7c716c45c2f9465915197d2499f032aa22f4c2
enzo405 <enzo.chaboisseau@gmail.com>
2026-05-17 19:48:34 +0200

    Add /show_blacklisted_channels command

--- bot/cogs/admin/channels.py
+++ bot/cogs/admin/channels.py
@@ -5,6 +5,8 @@ from typing import Union
 
 from bot.database import TableName
 
+_ADMIN_ONLY_MSG = "Only server administrator can use this command"
+
 
 class Channel(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -13,7 +15,7 @@ class Channel(commands.Cog):
 
     @app_commands.command(
         name="channel_whitelist",
-        description="Add the channel to the list of channel where the bot will interact",
+        description="Remove the channel from the blacklisted channels, allowing the bot to interact in it",
     )
     @app_commands.checks.has_permissions(administrator=True)
     async def channel_whitelist(
@@ -22,9 +24,7 @@ class Channel(commands.Cog):
         channel: Union[discord.TextChannel, discord.ForumChannel],
     ):
         if not interaction.user.guild_permissions.administrator:
-            await interaction.response.send_message(
-                "Only server administrator can use this command", ephemeral=True
-            )
+            await interaction.response.send_message(_ADMIN_ONLY_MSG, ephemeral=True)
             return
 
         if channel.id not in [
@@ -53,7 +53,7 @@ class Channel(commands.Cog):
 
     @app_commands.command(
         name="channel_blacklist",
-        description="Remove the channel to the list of channel where the bot can interact",
+        description="Add the channel to the blacklisted channels, preventing the bot from interacting in it",
     )
     @app_commands.checks.has_permissions(administrator=True)
     async def channel_blacklist(
@@ -62,9 +62,7 @@ class Channel(commands.Cog):
         channel: Union[discord.TextChannel, discord.ForumChannel],
     ):
         if not interaction.user.guild_permissions.administrator:
-            await interaction.response.send_message(
-                "Only server administrator can use this command", ephemeral=True
-            )
+            await interaction.response.send_message(_ADMIN_ONLY_MSG, ephemeral=True)
             return
 
         if channel.id in [
@@ -85,12 +83,49 @@ class Channel(commands.Cog):
             )
             self.bot.fetch_guilds_config()
             await interaction.response.send_message(
-                content="The channel <#"
-                + str(channel.id)
-                + "> will no longer check users message and give points when someone thanks another member.",
+                content=f"The channel <#{channel.id}> will no longer check users message and give points when someone thanks another member.",
                 ephemeral=True,
             )
         except discord.errors as e:
+            await interaction.response.send_message(e, ephemeral=True)
+
+    @app_commands.command(
+        name="show_blacklisted_channels",
+        description="Show all blacklisted channels for the server",
+    )
+    @app_commands.checks.has_permissions(administrator=True)
+    async def show_blacklisted_channels(
+        self,
+        interaction: discord.Interaction,
+    ):
+        if not interaction.user.guild_permissions.administrator:
+            await interaction.response.send_message(_ADMIN_ONLY_MSG, ephemeral=True)
+            return
+
+        blacklisted_ids = {
+            c["channel_id"]
+            for c in self.bot.guilds_config[interaction.guild.id]["blacklisted_channel"]
+        }
+
+        embed = discord.Embed(
+            title="Blacklisted Channels",
+            description="Here are the blacklisted channels for this server:",
+            color=discord.Color.blue(),
+        )
+        if not blacklisted_ids:
+            embed.add_field(
+                name="No blacklisted channels",
+                value="There are no blacklisted channels for this server.",
+                inline=False,
+            )
+        else:
+            embed.description = "Here are the blacklisted channels for this server:\n\n"
+            for channel_id in blacklisted_ids:
+                embed.description += f"<#{channel_id}>\n"
+
+        try:
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+        except discord.errors.HTTPException as e:
             await interaction.response.send_message(e, ephemeral=True)
 
 
